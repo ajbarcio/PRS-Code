d13c1c1dd0a22915090eedc6cd8c4e7bcd743e1c
unknown <ajbarcio@gmail.com>
2024-01-26 08:07:57 -0600

    20260126

--- ODE-Python/paper.py
+++ ODE-Python/paper.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.integrate import solve_ivp as ODE45
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 deg2rad = np.pi/180
@@ -53,8 +54,8 @@ def xyn(theta):
     xyn = np.array([xn, yn])
     return xyn
 
-def s(theta):
-    s = integral(sintegrand, theta)
+def s_exact(theta):
+    s = integral_theta(sintegrand, theta)
     return s
 
 def deriv_estimate(fun, theta):
@@ -65,7 +66,7 @@ def sintegrand(theta):
     sintegrand = np.sqrt(deriv_estimate(xyn, theta)[0]**2+deriv_estimate(xyn, theta)[1]**2)
     return sintegrand
 
-def integral(fun, theta):
+def integral_theta(fun, theta):
     #centered Riemman sum
     res = 1000
     step = (theta-thmin)/res
@@ -73,6 +74,17 @@ def integral(fun, theta):
     sum = 0
     while i < res:
         sum += fun(thmin+i*step)*2*step
+        i += 2
+    return sum
+
+def integral_s(fun, s):
+    #centered Riemman sum
+    res = 1000
+    step = (s)/res
+    i = 1
+    sum = 0
+    while i < res:
+        sum += fun(i*step)*2*step
         i += 2
     return sum
 
@@ -87,31 +99,92 @@ def gen_stiffness(theta):
         genStiffness = t*h*E*e(theta)*r_n(theta)
     return genStiffness
 
-def deform_ODE(theta, gamma):
+def stupid_cos(s):
+    return np.cos(alpha(theta_of_s_approx(s, *coeffs))+gamma_fun(s))
+
+def stupid_sin(s):
+    return np.sin(alpha(theta_of_s_approx(s, *coeffs))+gamma_fun(s))
+
+def xyc_deformed(s):
+    xyc = np.empty(2)
+    xyc[0] =  integral_s(stupid_cos, s)
+    xyc[1] =  integral_s(stupid_sin, s)
+
+def deform_ODE(s, gamma):
     Fx = F*np.sin(alpha(thmax))
     Fy = F*np.cos(alpha(thmax))
-    dgSds = deriv_estimate(gen_stiffness, theta)
+    theta = theta_of_s_approx(s, *coeffs)
+    dgSds = deriv_estimate(gen_stiffness, theta)/deriv_estimate(s_exact, theta)
     LHS = np.empty(2)
     LHS[0] = gamma[1]
     LHS[1] = (Fx*np.sin(alpha(theta)+gamma[0])-Fy*np.cos(alpha(theta)+gamma[0])-dgSds*gamma[1])/gen_stiffness(theta)
     return LHS
 
-print(c)
-print(np.linspace(thmin, thmax, 81))
+#print(c)
+# #print(np.linspace(thmin, thmax, 81))
 S = np.empty(81)
 for i in range(len(np.linspace(thmin, thmax, 81))):
-    S[i] = s(np.linspace(thmin, thmax, 81)[i])
+    S[i] = s_exact(np.linspace(thmin, thmax, 81)[i])
 
-# plt.plot(np.linspace(thmin, thmax, 81), S)
-print(S[-1])
+#print(S[-1])
+# print(np.column_stack([np.linspace(thmin, thmax, 81),S]))
 
-# F = 50
-# th_span = [thmin, thmax]
-# smax = s(thmax)
-# s_span  = [0, smax]
-# gamma = np.zeros(2)
-# dfmation = ODE45(deform_ODE, s_span, gamma, dense_output=True, options={'max_step': 1.0})
+def s_of_theta_approx(x, a, qn):
+    return (x-a)**qn-(thmin-a)**qn
 
-# plt.plot(dfmation.t, dfmation.sol(dfmation.t)[0])
-# print(dfmation.message)
+def theta_of_s_approx(x, a, qn):
+    return a + (x+(thmin-a)**qn)**(1./qn)
+
+coeffs, shit = curve_fit(s_of_theta_approx,np.linspace(thmin, thmax, 81),S,p0=[.52,1.695])
+# print(coeffs)
+
+# plt.plot((np.linspace(thmin, thmax, 81)), S)
+# plt.plot((np.linspace(thmin, thmax, 81)), s_of_theta_approx(np.linspace(thmin, thmax, 81),*coeffs))
+# plt.show()
+
+Fdeforms={}
+Fmeshes={}
+Fangles={}
+Frange = 5
+for i in range (-Frange,Frange+1):
+    if i==-Frange:
+        print("entered loop")
+    key=str("dfrm_F_"+str(i))
+
+    F = -i
+    th_span = [thmin, thmax]
+    smax = s_exact(thmax)
+    s_span  = [0, smax]
+    gamma = np.zeros(2)
+    print("thinking about what happens when F =",i)
+    tempDeform = ODE45(deform_ODE, s_span, gamma, dense_output=True, method='LSODA')
+    Fdeforms[key] = tempDeform.y[0,:]
+    Fmeshes[key]  = tempDeform.t
+    Fangles[key] = tempDeform.y[0,-1]
+
+plt.figure(1)
+for key in Fdeforms:
+    plt.plot(Fmeshes[key], Fdeforms[key])
+plt.figure(2)
+i = 0
+final_angles = np.empty(len(range(-Frange,Frange+1)))
+for key in Fangles:
+    final_angles[i]=Fangles[key]
+plt.plot(np.linspace(-Frange,Frange,len(range(-Frange,Frange+1))), final_angles)
+
+
+for key in Fmeshes:
+    print(key)
+for key in Fdeforms:
+    print(key)
+
+def gamma_fun(s):
+    return dfmation.sol(s)[0]
+
+# deformed_shape = np.empty([2,len(dfmation.t)])
+# for i in range(len(dfmation.t)):
+#     deformed_shape[:,i] = xyc_deformed(dfmation.t[i])
+
+# print(deformed_shape)
+
 plt.show()
