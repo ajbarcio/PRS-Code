b30aef4a8e6dcf7d78c5562092b93a9c179d374c
Anthony <ajbarcio@tamu.edu>
2024-09-17 08:49:20 -0500

    mostly testing symbolic algebra to solve ODE problem

--- ODE-Python/test.py
+++ ODE-Python/test.py
@@ -12,18 +12,37 @@ from utils import fixed_rk4, numerical_fixed_mesh_diff
 
 import sympy as sp
 
-x = np.array([[0],[float('nan')]])
-print(x)
-print(np.isfinite(x))
-print(np.isfinite(x).any())
-print(np.invert(np.isfinite(x)))
-print(np.invert(np.isfinite(x)).any())
-x = np.array([[0],[5]])
-print(x)
-print(np.isfinite(x))
-print(np.isfinite(x).any())
-print(np.invert(np.isfinite(x)))
-print(np.invert(np.isfinite(x)).any())
+a, b, r, t, I = sp.symbols('a,b,r,t,I')
+dI, dr        = sp.symbols('dI,dr')
+da, db        = sp.symbols('da,db')
+Q = sp.Matrix(2,2,[-a,b,(a/(r**2-a*r)),(b/(r**2+b*r))])
+QI = Q.inv()
+P = sp.Matrix(2,2,[1/(r*t),-I/(r**2*t),(0),(1/(r-a)-1/(r+b)-(a+b)/(r**2))])
+dI = t/2*((b**2-a**2)*dr+2*(b*db-a*da)*r)
+
+dp = sp.Matrix(2,1,[dI,dr])
+# print(dp)
+res = QI*P*dp
+print(sp.simplify(res[0]))
+print(sp.simplify(res[1]))
+
+print(sp.simplify(res[1])-sp.simplify(res[0]))
+
+# print(QI*P)
+# print(P[0,0])
+
+# x = np.array([[0],[float('nan')]])
+# print(x)
+# print(np.isfinite(x))
+# print(np.isfinite(x).any())
+# print(np.invert(np.isfinite(x)))
+# print(np.invert(np.isfinite(x)).any())
+# x = np.array([[0],[5]])
+# print(x)
+# print(np.isfinite(x))
+# print(np.isfinite(x).any())
+# print(np.invert(np.isfinite(x)))
+# print(np.invert(np.isfinite(x)).any())
 
 
 # x = sp.symbols('x')
